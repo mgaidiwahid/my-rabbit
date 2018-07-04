922852bee372f6206db1600acd6187335d771d65
mgaidiwahid <mgaidi.wahid@gmail.com>
2018-07-04 14:03:47 +0100

    add console to app

--- web/app.php
+++ web/app.php
@@ -12,7 +12,8 @@ if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
 //$kernel = new AppCache($kernel);
-
+$kernel = new AppKernel('prod', true); // Définissez ce 2e argument à true
+//
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();

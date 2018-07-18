ae88b62b4ad42af396691a0c6d3110e0402fd68b
mgaidiwahid <mgaidi.wahid@gmail.com>
2018-07-18 15:19:29 +0100

    add number misebas to dashboard

--- src/MR/BackOfficeBundle/Repository/MiseBasRepository.php
+++ src/MR/BackOfficeBundle/Repository/MiseBasRepository.php
@@ -10,10 +10,16 @@ class MiseBasRepository extends EntityRepository
     
    public function getCountMiseBas()
   { 
+
     return $this->createQueryBuilder('m')
                 ->select('COUNT(m)')
+                ->join('m.bande', 'b')
+                ->addSelect("b.libelle")
+                ->orderBy('m.bande', "DESC")
+                ->setMaxResults(1)
+                ->groupBy('m.bande')                              
                 ->getQuery()
-                ->getSingleScalarResult();
+                ->getResult();    
   } 
     
     
